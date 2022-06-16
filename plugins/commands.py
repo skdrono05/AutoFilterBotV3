1b47689dde7974974766c13e316d8f102e3815ba
Movie Show <105733634+skdrono05@users.noreply.github.com>
2022-06-16 15:29:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/+QoCiOv4LptpkYzM1")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "⭕️ SUPPORT ⭕️", url="https://t.me/+fOrMU4pmDFc3ZWQ1")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot")
+                            "SOURCE CODE", url="https://t.me/+fOrMU4pmDFc3ZWQ1")
                     ]
                 ]
             ),
